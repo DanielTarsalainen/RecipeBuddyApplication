737710a8039740d5c54b5f1c49907aceab9e7aff
Daniel <dj.tarsalainen@hotmail.com>
2021-04-13 22:39:38 +0300

    style changes

--- src/main/java/com/example/recipebuddyapp/WebSecurityConfig.java
+++ src/main/java/com/example/recipebuddyapp/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
+		http.authorizeRequests().antMatchers("/css/**").permitAll() 
 				.and().authorizeRequests().antMatchers("/recipelist", "/delete/{id}").permitAll().and()
 				.authorizeRequests().antMatchers("/signup", "/saveuser").permitAll().and().authorizeRequests()
 				.antMatchers("/uploadstatusview", "/upload").permitAll().and().authorizeRequests().anyRequest()
